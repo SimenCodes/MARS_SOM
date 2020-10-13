b530a2c8199f2893689fd2e775de6db98e1c5a25
M1U5T0N3 <mariuskopp517@gmail.com>
2020-10-13 23:47:59 +0200

    V201014
    -Added Apps and Content
    -Added Pixel 4 XL August Fingerprints
    -Removed Xperia Stock Apps
    -Removed unneeded MARS Content
    -Removed Xperia EU Stock Bloat
    -Reverted Xperia Home removing

--- customize.sh
+++ customize.sh
@@ -8,8 +8,19 @@ SKIPUNZIP=0
 
 REPLACE="
 /system/app/Stk
+/system/app/com.booking
+/system/app/com.epicgames.portal
+/system/app/com.facebook.appmanager
+/system/app/com.facebook.katana
+/system/app/com.gameloft.android.anmp.glofta9hm
+/system/app/com.sonymobile.support
+/system/priv-app/com.facebook.services
+/system/priv-app/com.facebook.system
+/system/priv-app/SemcAlbum-albumLivere-release
+/system/priv-app/SemcMusic
+/system/priv-app/SemcPhotoEditor
+/system/product/app/Music2
 /system/product/app/Stk
-/system/product/priv-app/Launcher3QuickStep
 "
 
 
