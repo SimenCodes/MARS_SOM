ebe5ecfb440d50a6b36c6cc922c1d8818d4be78a
M1U5T0N3 <mariuskopp517@gmail.com>
2020-10-13 21:53:19 +0200

    V201013
    TEST 3. Last Test Version Release...
    
    -Switched to own Installer
    -Removed a lot of Content again
    -Added Support for 2020 Xperias
    -Building MARS from Scratch again

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,7 @@
-# Don't modify anything after this
-if [ -f $INFO ]; then
-  while read LINE; do
-    if [ "$(echo -n $LINE | tail -c 1)" == "~" ]; then
-      continue
-    elif [ -f "$LINE~" ]; then
-      mv -f $LINE~ $LINE
-    else
-      rm -f $LINE
-      while true; do
-        LINE=$(dirname $LINE)
-        [ "$(ls -A $LINE 2>/dev/null)" ] && break 1 || rm -rf $LINE
-      done
-    fi
-  done < $INFO
-  rm -f $INFO
-fi
+#MARS
+
+remove_mars_dir() {
+  rm -rf /data/MARS
+}
+
+remove_mars_dir
