0f844f0387a918c8abd4a893d4e04b5853d7a51e
M1U5T0N3 <mariuskopp517@gmail.com>
2020-10-14 00:41:49 +0200

    V201014
    -Fixed typo
    -Added missing debloat Line

--- customize.sh
+++ customize.sh
@@ -13,10 +13,11 @@ REPLACE="
 /system/app/com.facebook.appmanager
 /system/app/com.facebook.katana
 /system/app/com.gameloft.android.anmp.glofta9hm
+/system/app/com.netflix.mediaclient
 /system/app/com.sonymobile.support
 /system/priv-app/com.facebook.services
 /system/priv-app/com.facebook.system
-/system/priv-app/SemcAlbum-albumLivere-release
+/system/priv-app/SemcAlbum-albumLive-release
 /system/priv-app/SemcMusic
 /system/priv-app/SemcPhotoEditor
 /system/product/app/Music2
